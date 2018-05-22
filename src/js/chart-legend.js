d92745b724ad82725ae32930fa55d6ee2c79ad04
alex <hamster2240@gmail.com>
2018-05-22 07:06:36 +0300

    Function for showing not empty legend item

--- src/js/chart-legend.js
+++ src/js/chart-legend.js
@@ -1,27 +1,35 @@
 function nameInput() {
   const inputName = document.querySelectorAll(".input-block__name");
   const nameText = document.querySelectorAll(".legend__item-name");
+  const legendItem = document.querySelectorAll(".legend__item");
   var nameVal = [];
   inputName.forEach(function(item) {
     nameVal.push(item.value);
   });
 
-  nameText.forEach(function(name, i) {
-    name.innerHTML = nameVal[i]
+  inputName.forEach(function(item, i) {
+    if (item.value !== "") {
+      legendItem[i].style.display = 'block'
+      nameText[i].innerHTML = nameVal[i]
+    } else {
+      legendItem[i].style.display = 'none'
+    }
   })
 }
 
 // color definition
 function chartLegend() {
   var legendSvg = d3.selectAll(".legend__item-color")
-                           .append("svg")
-                           .attr("width", 15)
-                           .attr("height", 15)
-                           .data(dataName)
-                           .attr('class', 'legend');
+    .append("svg")
+    .attr("width", 15)
+    .attr("height", 15)
+    .data(dataName)
+    .attr('class', 'legend');
 
-      legendSvg.append('rect')
-                .attr('width', 15)
-                .attr('height', 15)
-                .style("fill", function(d,i) { return color(i); });
+  legendSvg.append('rect')
+    .attr('width', 15)
+    .attr('height', 15)
+    .style("fill", function(d, i) {
+      return color(i);
+    });
 }
