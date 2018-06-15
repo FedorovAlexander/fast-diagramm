a5cb12b1b2bd80dbbdb6945558a4dbd21cbe7ef3
alex <hamster2240@gmail.com>
2018-06-15 22:03:09 +0300

    Add adding class "border-radius" to legend color svg to the second design function.

--- src/js/filters.js
+++ src/js/filters.js
@@ -3,15 +3,19 @@ const colorBtn1 = d3.selectAll(".filters__item--2");
 const result = document.querySelector('.result__output');
 const resultTitle = document.querySelector('.result__output-title');
 const legendItem = document.querySelectorAll('.legend__item');
-//toggle css class
+
+
 function secondDesign() {
+  d3.selectAll('.legend__color')
+    .attr('class', 'border-radius')
   resultTitle.classList.add('design-title-2');
   result.style.backgroundColor = '#fff';
+
   legendItem.forEach(function(item) {
     item.style.borderBottom = '1px solid #f0f0f0'
   })
-
 }
+
 //prevent default action
 document.querySelector('.filters__item--2').addEventListener('click', function(e){
   e.preventDefault()
