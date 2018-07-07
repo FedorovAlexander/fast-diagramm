220dd34d5cd3813f771f2f84d3aa5a3a2084bbf1
alex <hamster2240@gmail.com>
2018-07-07 13:03:17 +0300

    Make filters change a function

--- src/js/app.js
+++ src/js/app.js
@@ -83,3 +83,4 @@ function piechart() {
   }
   chartLegend();
 }
+filters()
--- src/js/filters.js
+++ src/js/filters.js
@@ -1,40 +1,40 @@
-const filtersBtn = d3.selectAll(".filters__item");
-const filtersBtnAll = document.querySelectorAll(".filters__item");
-const colorBtn1 = d3.selectAll(".filters__item--2");
-const result = document.querySelector('.result__output');
-const resultTitle = document.querySelector('.result__output-title');
-const legendItem = document.querySelectorAll('.legend__item');
+function filters() {
+  const filtersBtn = d3.selectAll(".filters__item");
+  const filtersBtnAll = document.querySelectorAll(".filters__item");
+  const colorBtn1 = d3.selectAll(".filters__item--2");
+  const result = document.querySelector('.result__output');
+  const resultTitle = document.querySelector('.result__output-title');
+  const legendItem = document.querySelectorAll('.legend__item');
+  var radius = Math.min(width, height) / 2;
 
+  function secondDesign() {
+    d3.selectAll('.legend__color')
+      .attr('class', 'border-radius')
+    resultTitle.classList.add('design-title-2');
+    result.style.backgroundColor = '#fff';
 
-function secondDesign() {
-  d3.selectAll('.legend__color')
-    .attr('class', 'border-radius')
-  resultTitle.classList.add('design-title-2');
-  result.style.backgroundColor = '#fff';
+    legendItem.forEach(function(item) {
+      item.style.borderBottom = '1px solid #f0f0f0'
+    })
+  }
 
-  legendItem.forEach(function(item) {
-    item.style.borderBottom = '1px solid #f0f0f0'
+  //prevent default action
+  filtersBtnAll.forEach(function(link) {
+    link.addEventListener('click',function(e) {
+      e.preventDefault()
+    })
   })
+
+  document.querySelector('.filters__item--2').addEventListener('click', function(){
+    secondDesign()
+  })
+
+  filtersBtn.on("click", function(_, i) {
+    d3.selectAll(".arc path").style("fill", function(_, j) {
+      return colorSet[i](j)
+    })
+    d3.selectAll(".legend__item-color rect").style("fill", function(_, j) {
+      return colorSet[i](j)
+    })
+  });
 }
-
-//prevent default action
-filtersBtnAll.forEach(function(link) {
-  link.addEventListener('click',function(e) {
-    e.preventDefault()
-  })
-
-})
-
-document.querySelector('.filters__item--2').addEventListener('click', function(e){
-  e.preventDefault()
-  secondDesign()
-})
-
-filtersBtn.on("click", function(_, i) {
-  d3.selectAll(".arc path").style("fill", function(_, j) {
-    return colorSet[i](j)
-  })
-  d3.selectAll(".legend__item-color rect").style("fill", function(_, j) {
-    return colorSet[i](j)
-  })
-});
