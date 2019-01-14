883acf19786de0284a5d1462e4ee2b8bcb903834
alex <hamster2240@gmail.com>
2019-01-14 13:22:06 +0300

    Function for the second design

--- src/js/filters.js
+++ src/js/filters.js
@@ -5,35 +5,60 @@ function filters() {
   const result = document.querySelector('.result__output');
   const resultTitle = document.querySelector('.result__output-title');
   const legendItem = document.querySelectorAll('.legend__item');
+
   var radius = Math.min(width, height) / 2;
 
+  function firstDesign() {
+    var itemColor = document.querySelectorAll('.legend__color')
+    itemColor.forEach(function (item) {
+      item.classList.add('border-radius-none')
+      item.classList.remove('border-radius')
+    })
+    resultTitle.classList.add('design-title-1');
+    resultTitle.classList.remove('design-title-2');
+    result.style.backgroundColor = '#fff';
+
+    legendItem.forEach(function (item) {
+      item.style.borderBottom = '1px solid transparent';
+
+    })
+  }
+
   function secondDesign() {
-    d3.selectAll('.legend__color')
-      .attr('class', 'border-radius')
+    var itemColor = document.querySelectorAll('.legend__color')
+    itemColor.forEach(function (item) {
+      item.classList.remove('border-radius-none')
+      item.classList.add('border-radius')
+    })
+    resultTitle.classList.remove('design-title-1');
     resultTitle.classList.add('design-title-2');
     result.style.backgroundColor = '#fff';
 
-    legendItem.forEach(function(item) {
+    legendItem.forEach(function (item) {
       item.style.borderBottom = '1px solid #f0f0f0'
     })
   }
 
   //prevent default action
-  filtersBtnAll.forEach(function(link) {
-    link.addEventListener('click',function(e) {
+  filtersBtnAll.forEach(function (link) {
+    link.addEventListener('click', function (e) {
       e.preventDefault()
     })
   })
 
-  document.querySelector('.filters__item--2').addEventListener('click', function(){
+  document.querySelector('.filters__item--1').addEventListener('click', function () {
+    firstDesign()
+  })
+
+  document.querySelector('.filters__item--2').addEventListener('click', function () {
     secondDesign()
   })
 
-  filtersBtn.on("click", function(_, i) {
-    d3.selectAll(".arc path").style("fill", function(_, j) {
+  filtersBtn.on("click", function (_, i) {
+    d3.selectAll(".arc path").style("fill", function (_, j) {
       return colorSet[i](j)
     })
-    d3.selectAll(".legend__item-color rect").style("fill", function(_, j) {
+    d3.selectAll(".legend__item-color rect").style("fill", function (_, j) {
       return colorSet[i](j)
     })
   });
